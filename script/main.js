a6b38caf2937caa17b0ff5b46fab06c1b97d5828
Emanuel Reis <emanuelemreis@gmail.com>
2023-05-04 22:07:31 -0300

    es

---
+++ script/main.js
@@ -0,0 +1,13 @@
+function menu() {
+    let menu = document.querySelector('span#menu')
+    let header = document.querySelector('header')
+    let logo = document.querySelector('#logo')
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+        header.style.display = 'block'
+        header.style.height = 'auto'
+        logo.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
