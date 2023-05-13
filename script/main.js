e32440bf33277890c4debf382cac19fa35f99fd3
Emanuel Reis <emanuelemreis@gmail.com>
2023-05-12 23:37:35 -0300

    cadastro

--- script/main.js
+++ script/main.js
@@ -1,10 +1,3 @@
-function menu(){
-    let opts = document.querySelector('#opts')
-    if (window.innerWidth <= '650px'){
-        opts.value.style.display = 'block'
-    }
-}
-
 function compra(){
     alert('~[ALERTA] Compra Indisponível no momento, tente novamente  mais tarde.')
 }
@@ -12,3 +5,18 @@ function compra(){
 function chamada(){
     alert('[ALERTA!] Fora de serviço no momento, por favor tente mais tarde')
 } 
+
+function validate(){
+    const infor = {}
+    infor.username = document.querySelector("#user");
+    infor.email = document.querySelector("#email");
+    infor.password = document.querySelector("#password");
+
+    if (infor.username.value == "" || infor.email.value == "" || infor.password.value == ""){
+        alert("[ERRO] Dados inválidos!");
+        return false;
+    } else {
+        alert("Dados cadastrados com sucesso!");
+        return true;
+    }
+}
