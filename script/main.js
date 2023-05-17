fdd919551199ec8762ed8a1b14d702af1ea68671
Emanuel Reis <emanuelemreis@gmail.com>
2023-05-17 00:18:33 -0300

    Responsividade do menu

--- script/main.js
+++ script/main.js
@@ -1,11 +1,25 @@
+// Responsividade do menu
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+hamburger.addEventListener("click", function() {
+    if (menu.style.display == "none") {
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
+})
+
+// Quando clicar nos planos
 function compra(){
     alert('~[ALERTA] Compra Indisponível no momento, tente novamente  mais tarde.')
 }
 
+// Opções de serviços
 function chamada(){
     alert('[ALERTA!] Fora de serviço no momento, por favor tente mais tarde')
 } 
 
+// Validar o formulário
 function validate(){
     const infor = {}
     infor.username = document.querySelector("#user");
